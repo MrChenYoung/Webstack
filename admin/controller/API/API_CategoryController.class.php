9c578b1697517b861e51b4539391e140bfc58dbb
code007 <yi_aohehe@163.com>
2020-07-08 03:13:18 +0800

    new

--- admin/controller/API/API_CategoryController.class.php
+++ admin/controller/API/API_CategoryController.class.php
@@ -130,7 +130,7 @@ class API_CategoryController extends API_BaseController
             if ($existSort){
                 $existSort = $existSort[0];
                 $oldsort = $oldsort[0];
-                echo "存在:".$existSort["id"]." catid:".$catId;
+                echo "存在:".$existSort["id"]." catid:".$oldsort["sort"]." dd:".$existSort["id"];
                 die;
                 DatabaseDataManager::getSingleton()->update($this->tableName,["sort"=>$oldsort["sort"]],["id"=>$existSort["id"]]);
             }
