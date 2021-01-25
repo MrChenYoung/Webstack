6496c8344af49727e2a5e74f8fae9d4d3cd06dfa
code007 <yi_aohehe@163.com>
2021-01-25 22:55:22 +0800

    new

--- admin/controller/DbBackup.php
+++ admin/controller/DbBackup.php
@@ -97,7 +97,7 @@ function backupDb($dataBase,$table="",$localTbPath,$logP,$tableDirName,$backupFi
     // 移动备份文件
 //    $cmd = "rclone moveto ".$localTbPath." GDSuite:我的数据/备份数据/db/".$dataBase."/".$tableDirName;
     $cmd = "move ".$localTbPath." ".$dbBackPathOnServer;
-    addLog($logP,"备份:".$cmd);
+    addLog($logP,"备份:".$localTbPath);
     return;
 
     $moveRes = myshellExec($cmd);
