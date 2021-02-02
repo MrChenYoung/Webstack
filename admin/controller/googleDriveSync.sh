4b276524056e77b9a0be4f5c9a009be252023cee
code007 <yi_aohehe@163.com>
2021-02-02 22:23:50 +0800

    new

--- admin/controller/googleDriveSync.sh
+++ admin/controller/googleDriveSync.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-#rclone sync /www/wwwroot/res.yycode.ml/backup BackupData:
-#rclone sync /www/wwwroot/res.yycode.ml/loveTheater LoveTheater:
-#rclone sync /www/wwwroot/res.yycode.ml/nextcloud NextCloud:
-#rclone sync /www/wwwroot/res.yycode.ml/test1 BackupData:
-#rclone sync /www/wwwroot/res.yycode.ml/test2 LoveTheater:
-rclone sync /www/wwwroot/res.yycode.ml/test2 LoveTheater:
-echo 12345 >> /www/wwwroot/res.yycode.ml/test1/test.txt
+rclone sync /www/wwwroot/res.yycode.ml/backup BackupData:
+rclone sync /www/wwwroot/res.yycode.ml/nextcloud NextCloud:
