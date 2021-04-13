0d0972b47e4ecfcdf45c17c5dc0c148b19736245
code007 <yi_aohehe@163.com>
2021-04-13 22:55:38 +0800

    new

--- admin/view/login/login.php
+++ admin/view/login/login.php
@@ -14,7 +14,7 @@
     <link href="<?php echo $pubP?>/common/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="<?php echo $pubP?>/common/css/icon.css" rel="stylesheet" type="text/css">
     <link href="<?php echo $pubP?>/common/css/login.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="//at.alicdn.com/t/font_1877015_4aj02q2orql.css">
+    <link rel="stylesheet" type="text/css" href="//at.alicdn.com/t/font_1877015_b05bhnqh5l.css">
 </head>
 
 <body>
